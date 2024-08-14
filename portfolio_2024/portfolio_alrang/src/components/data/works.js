f9e7b11cfbbf1d881986f9bf73b456cea79f9b49
alrang0929 <sellbell07@gmail.com>
2024-08-14 17:46:28 +0900

    포폴 ver2

--- portfolio_2024/portfolio_alrang/src/components/data/works.js
+++ portfolio_2024/portfolio_alrang/src/components/data/works.js
@@ -6,46 +6,80 @@
 
 idx: 프로젝트 구분키
 title: 프로젝트 타이틀
+engtitle: 프로젝트 타이틀
 category: 프로젝트 카테고리
 info: 프로젝트 날짜^사용한 툴 << splice로 컷팅하여 셋팅
 isrc: 배경이미지 링크 (ex:`url('/images/Rectangle_27.jpg')`)
 *************************************************************/
 
 const worksData = [
-    {
-        idx: 1,
-        title: '아모레퍼시픽 UI/UX Recover',
-        category: 'UX / UI design, fe Development',
-        info: '2024.04.15^React, jQuery, SCSS, HTML5',
-        text:`아모레퍼시픽의 기존 사이트를 트랜드에 맞게 ui와 ux를 리디자인 했다.
+  {
+    idx: 1,
+    title: "아모레퍼시픽 UI/UX Recover",
+    engtitle: "Amorepacific UI/UX Recover",
+    category: "UX / UI design, fe Development",
+    info: "2024.04.15^React, jQuery, SCSS, HTML5",
+    text: `아모레퍼시픽의 기존 사이트를 트랜드에 맞게 ui와 ux를 리디자인 했다.
 cross over한 사이트는 최근 리브랜딩 사이트를 공개한 정관장을 택했으며
 브랜드 소개 페이지 답게 기존의 사이트 보다 쉽고 직관적으로 브랜드 가치를 경험할 수 있도록 했으며
 대표 상품을 노출, 소개하여 아모레퍼시픽 내 각 브랜드들의 이미지를 부각시켰다`,
-        isrc: {bg:"/images/amr_bg.jpg",ipage:"/images/amr_pg.png"},
-        url: {pdf: "https://drive.google.com/file/d/1J0x0dtpHVRxKqZ03WT9dPgYAteJ_-fqg/view?usp=sharing", page:"https://alrang0929.github.io/FED-RF-2th-PJ-jihyeon/1%EC%B0%A8%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/"},
+    isrc: {
+      bg: "/images/amr_bg.jpg",
+      mainlist: "/images/main/PC_project_side_amore.jpg",
+      workslist: "/images/main/PC_main_list_amore.jpg",
+      ipage: "/images/amr_pg.png",
     },
-    {
-        idx: 2,
-        title: '슬밋 UI/UX Recover',
-        category: 'UX / UI design, fe Development',
-        info: '2024.07.28^React^jQuery, SCSS, HTML5',
-        text:`리액트SPA환경에서 작업한 프로젝트,
+    url: {
+      pdf: "https://drive.google.com/file/d/1J0x0dtpHVRxKqZ03WT9dPgYAteJ_-fqg/view?usp=sharing",
+      page: "https://alrang0929.github.io/FED-RF-2th-PJ-jihyeon/1%EC%B0%A8%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/",
+    },
+  },
+  {
+    idx: 2,
+    title: "슬밋 UI/UX Recover",
+    engtitle: "Seulmit UI/UX Recover",
+    category: "UX / UI design, fe Development",
+    info: "2024.07.28^React^jQuery, SCSS, HTML5",
+    text: `리액트SPA환경에서 작업한 프로젝트,
 리액트의 강점인 대량의 데이터처리, 데이터 재활용에 중점을 두고
-코드 설계를 했다`,
-        isrc: {bg:"/images/sm_bg.jpg",ipage:"/images/seulmit_pg.png"},
-        url: {pdf: "https://drive.google.com/file/d/1lTuTRm9JZ9qt6WGas2uKq6mgFRPUvaXX/view?usp=sharing", page:"https://alrang0929.github.io/FED-RF-2th-PJ-jihyeon/2%EC%B0%A8%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/seulmit-app/build/"},
+코드 설계를 했다. 검색, 로그인, 장바구니 기능은 레지스토리를 활용하여 구현`,
+    isrc: {
+      bg: "/images/sm_bg.jpg",
+      mainlist: "/images/main/PC_project_side_seumit.jpg",
+      workslist: "/images/main/PC_main_list_seumit.jpg",
+      ipage: "/images/seulmit_pg.png",
     },
-    {
-        idx: 3,
-        title: 'STAR WARS 팀 프로잭트 서브페이지 BAD BATCH',
-        category: 'UX / UI design, fe Development',
-        info: '2024.04.22^React, jQuery, SCSS, HTML5',
-        text:`팀프로잭트이니 만큼 공통소스 설계에 중점을 뒀다.
+    url: {
+      pdf: "https://drive.google.com/file/d/1lTuTRm9JZ9qt6WGas2uKq6mgFRPUvaXX/view?usp=sharing",
+      page: "https://alrang0929.github.io/FED-RF-2th-PJ-jihyeon/2%EC%B0%A8%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/seulmit-app/build/",
+    },
+  },
+  {
+    idx: 3,
+    title: "STAR WARS 팀 프로잭트 서브페이지 BAD BATCH",
+    engtitle: "STAR WARS Team Project Subpage BAD BATCH",
+    category: "UX / UI design, fe Development",
+    info: "2024.04.22^React, jQuery, SCSS, HTML5",
+    text: `팀프로잭트이니 만큼 공통소스 설계에 중점을 뒀다.
 공통으로 사용되는 소스로는 [컬러(포인트, 텍스트, 디사블) / 폰트(타이틀, 서브타이틀, 텍스트) / 버튼 디자인(노말, 엑티브, 디사블), 하단 타 서브타이틀 이동 배너 컴포넌트]가 있으며
 공통으로 사용되는 디자인은 회의를 통해 디자인 class 를 작성 후 공통으로 사용하도록 진행했다.`,
-        isrc: {bg:"/images/starwars_bg.jpg",ipage:"/images/star_pg.png"},
-        url: {pdf: "https://docs.google.com/presentation/d/1MibZw2WQNsFUq7TPcNIZ10EAOOt63oFwqapo76ChAvQ/edit?usp=sharing", page:"https://alrang0929.github.io/FED_2rd_star_wars/000_React_PJ/star-app/build"},
+    isrc: {
+      bg: "/images/starwars_bg.jpg",
+      mainlist: "/images/main/PC_project_side_star.jpg",
+      workslist: "/images/main/PC_main_list_star.jpg",
+      ipage: "/images/star_pg.png",
     },
-]
+    url: {
+      pdf: "https://docs.google.com/presentation/d/1MibZw2WQNsFUq7TPcNIZ10EAOOt63oFwqapo76ChAvQ/edit?usp=sharing",
+      page: "https://alrang0929.github.io/FED_2rd_star_wars/000_React_PJ/star-app/build",
+    },
+  },
+];
 
-export default worksData;
+const worksThumbs = [
+  { isrc: "/images/main/PC_project_thumb_amore.jpg", key: "amore" },
+  { isrc: "/images/main/PC_project_thumb_seumit.jpg", key: "seumit" },
+  { isrc: "/images/main/PC_project_thumb_star.jpg", key: "star" },
+];
+// export default worksData;
+export { worksData, worksThumbs };
--- portfolio_2024/portfolio_alrang/src/components/pages/Main.jsx
+++ portfolio_2024/portfolio_alrang/src/components/pages/Main.jsx
@@ -1,131 +1,282 @@
 //portfolio pj - Main컴포넌트
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import $ from "jquery";
+import React, { useEffect, useRef, useState } from "react";
+import { Form, Link } from "react-router-dom";
+import $, { event } from "jquery";
 //css
 import "../../css/main.scss";
 //data
-import {skilList} from "../data/skil"; 
-
+import { skillList } from "../data/skill";
+import { worksData, worksThumbs } from "../data/works";
 ///////////////////import area//////////////////
 function Main() {
+  const titleWrapRef = useRef(null);
+  const [images, setImages] = useState([]);
 
-    //상태관리변수
-    // 1. 애니 상태관리변수
-    const [isAni, setIsAni] = useState(false);
+  console.log("미친데이터", worksData);
 
-    //화면 랜더링 구역////////////////////
-    useEffect(() => {
+  //화면 랜더링 구역/////////////////////////////////////////////////
+  useEffect(() => {
+    // 타이틀 상단 랜덤 이미지 등장
 
-        // 스크롤 위치 도달시 애니
-        const handleScroll =()=>{
-            //대상선정
-            const aniObject = $("#profile-area .profile-wrap");
-            //윈도우, 스크롤 영역 변수
-            const scrollTop = $(window).scrollTop();
-            const windowHeight = $(window).height();
-            const triggerPoint = windowHeight / 2;
+    const handleMouseMove = (e) => {
+      if (titleWrapRef.current && titleWrapRef.current.contains(e.target)) {
+        // 이미지 배열에서 랜덤 이미지 선택
+        const randomImage = new Image();
+        randomImage.src = `/images/random_img/random_0${
+          Math.floor(Math.random() * 4) + 1
+        }.jpg`; // 이미지 경로와 개수 수정 필요
 
-            if(scrollTop > triggerPoint && !isAni){
-                aniObject.animate({
-                    opacity: 1,
-                    marginTop: 0,
-                },1000);
-                setIsAni(true);
-            }//if
-        };
-        $(window).on("scroll", handleScroll);
-        return()=>{
-            $(window).off("scroll", handleScroll);
-        }
-        
-    }, [isAni]);
+        // 이미지 스타일 설정 및 추가
+        $(randomImage)
+          .css({
+            position: "absolute",
+            left: e.clientX + "px",
+            top: e.clientY + "px",
+            width: "128px",
+            height: "150px",
+            objectFit: "cover",
+            zIndex: 1,
+            // 애니를 위한 사전 설정
+            opacity: 0,
+          })
+          .animate(
+            {
+              opacity: 1,
+            },
+            300,
+            () => {
+              // 마우스 나가면 op 초기화
+              $(randomImage).animate(
+                {
+                  opacity: 0,
+                },
+                300
+              );
+            }
+          );
 
-    ///코드 리턴구역 /////////////////////////////////////////////////////////
-    return (
-        <main id="main-area">
-            {/* 1. 메인 비쥬얼 영역 */}
-            <section id="main-visual-area">
-                {/* 1)텍스트 박스 : 타이틀, 텍스트 */}
-                <div className="text-wrap">
-                    <h2 className="title gilda-display-regular">Alrang’s</h2>
-                    <h2 className="title gilda-display-regular">Work Place</h2>
-                    <div className="text-box">
-                        <span className="text">
-                        A new beginning combines three-dimensional excitement and fear, but
-                        </span>
-                        <span className="text">
-                        when you take courage and let go, your mouse begins to grow in an instant.
-                        </span>
-                     
-                    </div>
+        titleWrapRef.current.appendChild(randomImage);
+        setImages([...images, randomImage]); // 이미지 배열에 추가
+
+        // 잠시 후 이미지 제거
+        setTimeout(() => {
+          randomImage.remove();
+          setImages(images.filter((img) => img !== randomImage));
+        }, 1000); // 1초 후 제거 (시간 조절 가능)
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    //코드 정리
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      // 컴포넌트 언마운트 시 이미지들 제거
+      images.forEach((img) => img.remove());
+    };
+  }, [images]); // images 상태가 변경될 때마다 useEffect 실행
+
+  ///코드 리턴구역 /////////////////////////////////////////////////////////
+  return (
+    <main id="main-area">
+      {/* 1. 메인 슬라이드 */}
+      <div id="main-visual-area">
+        {/* 1) 타이틀 박스 */}
+        {/* 2) mouse leave시 등장하는 이미지 박스 : ref값에 배열 저장하여 액팅 */}
+        <div className="title-wrap gilda-display-regular" ref={titleWrapRef}>
+          <span>Alrang’s</span>
+          <span>Work Place</span>
+        </div>
+        {/* 3) works list 리스트 */}
+        <div className="works-list-wrap fxbox">
+          <div className="title">selected project</div>
+          <ul className="works-list fxbox">
+            {worksThumbs.map((v, i) => (
+              <li key={i}>
+                <a href="">
+                  <div className="imgbx">
+                    <img src={process.env.PUBLIC_URL + v.isrc} alt={v.key} />
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* 4) works list 호버시 우측에서 등장하는 이미지 */}
+        <div className="show-works-box">
+          <ul className="fxbox">
+            {worksData.map((v, i) => (
+              <li key={i}>
+                <a href="">
+                  <div className="title">{v.title}</div>
+                  <div className="imgbx">
+                    <img
+                      src={process.env.PUBLIC_URL + v.isrc.mainlist}
+                      alt={v.title}
+                    />
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {/**************** 2. 프로필 영역: 인삿말, 스킬, 인포 ****************/}
+      <div id="profile-area">
+        <div id="intro-area">
+          {/* 1) 인삿말 */}
+          <div className="hello-text-wrap">
+            {/* 1)hello-img */}
+            <div className="imgbx">
+              <img
+                src={process.env.PUBLIC_URL + "/images/main/PC_hello.jpg"}
+                alt="hello"
+              />
+            </div>
+            {/* 2)back-text-bubbles*/}
+            <div className="back-text-bubbles-wrap">
+              <ul>
+                <li>
+                  <span>
+                    오늘 <b>점심</b> 뭐먹지
+                  </span>
+                </li>
+                <li>
+                  <span>
+                    <b>commit</b> 하기 전에 생각했나요?
+                  </span>
+                </li>
+                <li>
+                  <span>저장저장..</span>
+                </li>
+                <li>
+                  <span>
+                    이 <b>에러</b>의 정체는...??^ㅠ
+                  </span>
+                </li>
+              </ul>
+              <div className="introduction">
+                안녕하세요, 언제나 고민하는
+                <b>프론트앤드 개발자 김지현</b>입니다.
+              </div>
+            </div>
+            {/* 3)BG acting text */}
+            <div className="ani-text gilda-display-regular">
+              Alrang’s Work Place
+            </div>
+          </div>
+          {/* 2) 스킬트리 */}
+          <div className="skill-wrap">
+            <div className="title"></div>
+            <ul className="skill-list fxbox">
+              {skillList.map((v, i) => (
+                <li className="fxbox" key={i}>
+                  <div className="icon">
+                    <img
+                      src={process.env.PUBLIC_URL + v.isrc}
+                      alt={v.skillName}
+                    />
+                  </div>
+                  <div className="text">{v.skillName}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* 3) 인포 텍스트 */}
+          <div className="info-text-wrap">
+            <div className="big-title">
+              <span> connecting</span>
+              <span>the dots</span>
+            </div>
+            <div className="img-wrap fxbox">
+              <div className="imgbx">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/main/PC_main_jabs.jpg"}
+                  alt="스티븐 잡스"
+                />
+              </div>
+              <div className="text">
+                스티븐 잡스 유명한 격언이자 지금의 제가 있게 해준 격언입니다.
+                웹이 좋아 웹디자이너로 이 세계에 발을 들이고 팀 구성원들,
+                사용자들과 소통을 하다 생긴 물음표를 해결해나가다보니 사용자들과
+                가장 밀접한 상호관계를 가지고 있는 프론트앤드 개발자로 전향을
+                결심하게 되었습니다.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*********************** 2. 프로필영역 끝 ***********************/}
+      {/* 3. works 영역: works list */}
+      <div id="works-list-area">
+        {/* ul은 그리드로 구성, 12col, a는 각 프로젝트로 링크 */}
+        <ul>
+          {worksData.map((v, i) => (
+            <li key={i}>
+              <a href="">
+                <div className="img-wrap">
+                  <div className="eng-title gilda-display-regular">
+                    {v.engtitle}
+                  </div>
+                  <div className="kor-title">{v.title}</div>
+                  <div className="imgbx">
+                    <img
+                      src={process.env.PUBLIC_URL + v.isrc.workslist}
+                      alt={v.title}
+                    />
+                  </div>
                 </div>
-                {/* 2)스크롤 다운 : 아이콘, 텍스트*/}
-                <div className="scroll-down">
-                    <div className="ani-bar"></div>
-                    <span>SCROLL DOWN </span>
-                </div>
-            </section>
-            {/* 2. 프로필 영역 */}
-            <section id="profile-area">
-                {/* 1) 이미지 박스 */}
-                <div className="profile-wrap">
-                    <div className="imgbx">
-                        <img
-                            src={
-                                process.env.PUBLIC_URL + "/images/main_img.jpg"
-                            }
-                            alt="메인 이미지"
-                        />
-                    </div>
-                    {/* 2) 자기소개 : 타이틀, 텍스트 */}
-                    <div className="text-wrap">
-                        <h4 className="title gilda-display-regular">
-                            About Me
-                        </h4>
-                        <span className="text">
-                        웹이 좋아 시작하게된 웹디자이너, 언제부턴가 시각적인 것들 뿐만 아니라 그 뒤에 있는 개발자의 영역이 궁금해졌습니다. 그래서 같이 프로잭트를 진행하던 프론트앤드의 뒤를 기웃거리며 호기심을 하나 둘 채워가다 프론트엔드 개발자에 도전하게 되었습니다.
-                        디자인 기획 경험을 통해 프로젝트 전반에 대한 이해도가 높으며, 팀원들과 함께 문제를 분석하고 해결하는 능력을 갖추고 있다고 생각합니다. 이러한 강점은 디자이너, 클라이언트들과 소통과 협업을 할 때 좋은 시너지를 가져올 것이라고 자신합니다.
-                        </span>
-                    </div>
-                    {/* 3) 스킬트리 : [타이틀, 게이지] * n*/}
-                </div>
-                    <div className="skil-list-wrap">
-                    <h4 className="title gilda-display-regular">
-                            About Skil
-                        </h4>
-                        <ul className="skil-list fxbox">
-                           {skilList.map((v,i)=>
-                            <li key={i} className="fxbox">
-                                <div className="icon-box">
-                               <img src={process.env.PUBLIC_URL +v.isrc} alt="" />
-                                </div>
-                                <span className="icon-text">{v.skillName}</span>
-                            </li>
-                           )}
-                        </ul>
-                    </div>
-            </section>
-            {/* 3. CONTACT US */}
-            <section id="contact-area">
-                {/* 1)텍스트박스 : 타이틀, 이메일, 텍스트, 밑줄 요소 */}
-                <div className="text-box">
-                    <h3 className="title gilda-display-regular">CONTACT US</h3>
-                    <div className="email gilda-display-regular">
-                        duudaa0939@gmail.com
-                    </div>
-                    <div className="text">
-                    If you are curious about me? here ▼
-                    </div>
-                    <span className="under-line-w"></span>
-                </div>
-                {/* 2) 포트폴리오 이동 링크 */}
-                <Link to="/Works" className="gilda-display-regular">
-                    Works more view
-                </Link>
-            </section>
-        </main>
-    );
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* 4. conect us */}
+      <div id="conect-us-area">
+        {/* 1) 얼굴 이미지 */}
+        <div className="imgbx">
+          <img
+            src={process.env.PUBLIC_URL + "/images/main/PC_conectus.jpg"}
+            alt="conect face"
+          />
+        </div>
+        {/* 2) 타이틀 */}
+        <div className="title">CONTACT US</div>
+        {/* 3) 정보 */}
+        <ul>
+          <li className="fxbox">
+            <div className="icon-box">
+              <div className="icon">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/icon/icon_mail.png"}
+                  alt="email"
+                />
+              </div>
+              <div className="text">eMail</div>
+            </div>
+            <div className="text">sellbell0929@gmail.com</div>
+          </li>
+          <li className="fxbox">
+            <div className="icon-box">
+              <div className="icon">
+                <img
+                  src={process.env.PUBLIC_URL + "/images/icon/icon_git.png"}
+                  alt="git"
+                />
+              </div>
+              <div className="text">Github</div>
+            </div>
+            <div className="text">
+              https://github.com/alrang0929/alrang_portfolio
+            </div>
+          </li>
+        </ul>
+      </div>
+      {/* 저작권 표시 */}
+      <div className="copy">© 2024 Alrang. All rights reserved.</div>
+    </main>
+  );
 }
 
 export default Main;
